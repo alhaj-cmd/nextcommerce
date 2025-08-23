3272afc083add38c8e60e5a8920fbd4bd1274892
alhaj-cmd <alhazsarkar5523@gmail.com>
2025-08-23 22:17:44 +0600

    folder structer setting

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPages(){
+
+    return (
+        <div>
+            <h2>Hello About pages</h2>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      {/* <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -50,10 +50,8 @@ export default function Home() {
             Read our docs
           </a>
         </div>
-      </main>
-      <div className="header">
-        <header/>
-      </div>
+      </main> */}
+      <h1>This is the Home pages</h1>
     </div>
   );
 }
