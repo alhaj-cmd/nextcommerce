ad12de4a7dff295fd158a22a7bb854ea677d9809
alhaj-cmd <alhazsarkar5523@gmail.com>
2025-08-28 22:40:18 +0600

    class function use

--- app/contract/page.tsx
+++ app/contract/page.tsx
@@ -1,7 +1,7 @@
 export default function Contracpackes(){
     return(
         <div>
-            <h2>My Contract pages with my info</h2>
+            <h2 className="text-red-100">My Contract pages with my info</h2>
         </div>
     )
 
